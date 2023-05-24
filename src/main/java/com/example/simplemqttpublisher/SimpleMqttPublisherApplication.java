ee8e855ecc8143a74151307a7dd5b6513e00d7a2
Foivos Paraskevas <fparask@cti.gr>
2023-05-24 12:00:45 +0300

    Initialization of project

---
+++ src/main/java/com/example/simplemqttpublisher/SimpleMqttPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplemqttpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMqttPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMqttPublisherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplemqttpublisher/SimpleMqttPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplemqttpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMqttPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
